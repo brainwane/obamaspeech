7333432d7cc6eacdc5057856ba77033e3565b40c
Sumana Harihareswara <sumanah@panix.com>
2013-10-22 11:17:54 -0400

    more tests

--- speech.py
+++ speech.py
@@ -27,3 +27,30 @@ def test_invalid_item_insertion():
     #Insert an invalid item into a fake list and ensure it gives the right
     #errors: "that's not English" if it's not all alphabetical characters,
     #"try again; what did you mean?" if it's empty. Rerun the raw_input.
+    pass
+
+def test_get_bug():
+    #Check that we can actually get a bug via the API.
+    pass
+
+def test_extract_bug_info():
+    #Check whether, given a mock bug, we can extract the bug number, title,
+    #and reporter name.
+    pass
+
+def test_insert_bug_number():
+    #Given a mock bug # and a mock template, check that inserting the bug #
+    #into the template gives us a template with the bug # filled in.
+    pass
+
+def test_insert_bug_title():
+    #Given a mock bug title and a mock template, check that inserting the bug
+    #title into the template gives us a template with the bug title filled in.
+    pass
+
+def test_insert_bug_reporter():
+    #Given a mock bug reporter and a mock template, check that inserting the
+    #reporter name into the template gives us a template with the bug
+    #reporter's name filled in.
+    pass
+
