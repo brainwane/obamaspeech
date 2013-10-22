854c897e7dbbbc152992e28369065a4b3689af96
Sumana Harihareswara <sumanah@panix.com>
2013-10-22 13:45:42 -0400

    using unittest module, python3

--- speech.py
+++ speech.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
 """
-This is a fake speech generator.
+This is a fake speech generator. I'm trying to do Python 3.
 
 It should go something like:
 
@@ -17,40 +16,66 @@ Thank you, and may God bless {another tech buzzword}.
 
 {Flag shot|"We now return you to your regularly scheduled programming"|Bugle tones}
 """
+import unittest
 
-def test_valid_item_insertion():
+class item_insertion_test_case(unittest.TestCase):
+    def test_valid_item_insertion(self):
     #Try taking a fake list and make sure that when we insert a new valid item,
     #it works.
-    pass
+        pass
 
-def test_invalid_item_insertion():
+    def test_invalid_item_insertion(self):
     #Insert an invalid item into a fake list and ensure it gives the right
     #errors: "that's not English" if it's not all alphabetical characters,
     #"try again; what did you mean?" if it's empty. Rerun the raw_input.
-    pass
+        pass
 
-def test_get_bug():
+class bug_test_case(unittest.TestCase):
+
+    def test_get_bug(self):
     #Check that we can actually get a bug via the API.
-    pass
+        pass
 
-def test_extract_bug_info():
+    def test_extract_bug_info(self):
     #Check whether, given a mock bug, we can extract the bug number, title,
     #and reporter name.
-    pass
+        pass
 
-def test_insert_bug_number():
+    def test_insert_bug_number(self):
     #Given a mock bug # and a mock template, check that inserting the bug #
     #into the template gives us a template with the bug # filled in.
-    pass
+        pass
 
-def test_insert_bug_title():
+    def test_insert_bug_title(self):
     #Given a mock bug title and a mock template, check that inserting the bug
     #title into the template gives us a template with the bug title filled in.
-    pass
+        pass
 
-def test_insert_bug_reporter():
+    def test_insert_bug_reporter(self):
     #Given a mock bug reporter and a mock template, check that inserting the
     #reporter name into the template gives us a template with the bug
     #reporter's name filled in.
-    pass
+        pass
 
+class buzzword_test_case(unittest.TestCase):
+
+    def test_buzzword_grab(self):
+    #Grab a buzzword from the list.
+        pass
+
+    def test_buzzword_insert(self):
+    #Check that the buzzword inserted into the template is the one you 
+    #wanted to insert.
+        pass
+
+class output_test_case(unittest.TestCase):
+
+    def test_print_to_console(self):
+    #not sure how to test this.
+        pass
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
